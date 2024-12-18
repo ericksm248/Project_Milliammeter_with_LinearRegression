d7cfa3a5fb5292fe7c5f9e155d6319dff69ebc01
ericksm248 <147954327+ericksm248@users.noreply.github.com>
2024-12-18 10:57:13 -0500

    Update least_squares.py

--- least_squares_python/least_squares.py
+++ least_squares_python/least_squares.py
@@ -6,18 +6,22 @@ y = np.array([3.3,4.1,5.3,6.5,7.5,8.5,13,21.7,33,77,120,208,295,382,471.5,508,69
 
 prom_x = x.mean()
 prom_y = y.mean()
-n = len(x)
-a=(n*(sum(x*y))-sum(x)*sum(y))/(n*(sum(x**2))-(sum(x)**2))
-b = prom_y -a*prom_x
+sum_numerator = 0
+sum_denominator = 0
+for i,j in zip(x,y):
+    sum_numerator += i*(j-prom_y)
+    sum_denominator += i*(i-prom_x)
 
-print(f"y = {a:.8f}x + {b:.8f}")
+m = sum_numerator/sum_denominator
+b = prom_y -m*prom_x
+print(f"y = {m:.8f}x + {b:.8f}")
 
 idx_orden = np.argsort(x)
 plt.plot(sorted(x),y[idx_orden],"o")
-y = a*(np.linspace(1,600)) + b
+y = m*(np.linspace(1,600)) + b
 plt.plot(np.linspace(1,600),y)
 plt.title("Trend Line")
 plt.ylabel("Milliamps Standar Instrument")
 plt.xlabel("ADC read from PIC")
-plt.legend(["Dispersed points",f"straight line found : y = {a:.4f}x + {b:.4f}"])
+plt.legend(["Dispersed points",f"Straight Line found : y = {m:.4f}x + {b:.4f}"])
 plt.show()
